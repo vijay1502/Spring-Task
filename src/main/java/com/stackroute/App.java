6900b28314184ab727cf15af49af8f7257128ab6
vijay <vijaya.kanugovi@cgi.com>
2019-07-28 23:20:08 +0530

    task3 completed

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -12,19 +12,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App 
 {
     public static void main( String[] args )
-    {ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Actor actor1=context.getBean("actor1",Actor.class);
-        Actor actor2=context.getBean("actor2",Actor.class);
-        Actor actor3=context.getBean("actor3",Actor.class);
-        Movie movie1=context.getBean("movie1",Movie.class);
-        Movie movie2=context.getBean("movie2",Movie.class);
+    { ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        Movie actor=context.getBean("movie1", Movie.class);
+        Movie actor1=context.getBean("movie2",Movie.class);
+        Movie actor2=context.getBean("movie3",Movie.class);
 
+        System.out.println(actor.toString());
         System.out.println(actor1.toString());
         System.out.println(actor2.toString());
-        System.out.println(actor3.toString());
-        System.out.println(actor1==actor2);
-        System.out.println(movie1.getActor1());
-        System.out.println(movie2.getActor2());
-        System.out.println(movie1==movie2);
+
+
     }
 }
--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,17 +1,16 @@
 package com.stackroute.domain;
 
-public class Actor {
-    private String name;
+public class Actor {private String name;
     private String gender;
     private int age;
-
-    public Actor() {
-    }
 
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
+    }
+
+    public Actor() {
     }
 
     public String getName() {
@@ -37,14 +36,4 @@ public class Actor {
     public void setAge(int age) {
         this.age = age;
     }
-
-    @Override
-    public String toString() {
-        return "Actor{" +
-                "name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
 }
